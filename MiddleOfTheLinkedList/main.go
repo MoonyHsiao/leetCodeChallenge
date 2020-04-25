aef70f53f462f0c25ebefb6a89eb7b9c0a619799
Ivan Hsiao <moonysc2@gmail.com>
2020-04-25 15:24:42 +0800

    feat(add insert data): add insert data

--- MiddleOfTheLinkedList/main.go
+++ MiddleOfTheLinkedList/main.go
@@ -9,18 +9,25 @@ type ListNode struct {
 	Next *ListNode
 }
 
+var head *ListNode
+
 func main() {
 
-	// arr := []int{1, 2, 3, 4, 5}
+	arr := []int{1, 2, 3, 4, 5, 6}
 
-	head := new(ListNode)
-	head.Val = 1
-	temp := new(ListNode)
-	temp.Val = 2
-	head.Next = temp
-	temp2 := new(ListNode)
-	temp2.Val = 3
-	temp.Next = temp2
+	// head := &ListNode{
+	// 	Val: 1,
+	// 	Next: &ListNode{
+	// 		Val: 2,
+	// 		Next: &ListNode{
+	// 			Val: 3,
+	// 		},
+	// 	},
+	// }
+
+	for i := range arr {
+		addTestData(arr[i])
+	}
 
 	res := middleNode(head)
 	fmt.Printf("res:%v", res)
@@ -45,4 +52,19 @@ func middleNode(head *ListNode) *ListNode {
 	}
 
 	return res
+}
+
+func addTestData(x int) {
+	s := &ListNode{
+		Val: x,
+	}
+	if head == nil {
+		head = s
+	} else {
+		currentNode := head
+		for currentNode.Next != nil {
+			currentNode = currentNode.Next
+		}
+		currentNode.Next = s
+	}
 }
